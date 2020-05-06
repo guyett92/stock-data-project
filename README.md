33026befdfc7224ec41998ad1607ffcbe995ca30
guyett92 <aaronguyett@yahoo.com>
2020-05-05 19:15:28 -0700

    creates template and readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Stock Market Data App v0.01
+## Technologies Used
+* HTML
+* CSS
+* JavaScript
+* jQuery
+* AJAX
+## Features
+Users can search for the current price of a stock by using its associated acronym. The application will return the stock's full name and daily change as well. 
+The application pulls data from multiple APIs to serve to users once they have made a request.
+## User Stories
+* Users can request to view the current value of a stock
+* Users can identify the name of a stock through the acronym.
+* Users will be able to view a logo if it is in a the ClearBit logo API.
+## Wireframe
+[Balsamiq Wireframe](https://balsamiq.cloud/siuhwm9/psuebbq)
+## Pseudocode
+[Pseudocode](https://drive.google.com/file/d/14o57qOiqKLFNRfv4N-TSPakvKmoaIogk/view?usp=sharing)
+## Goals
+* Allow a user to search with the name of a company OR its acronym.
+* Add a currency selector to the application.
+* Add a way to guarantee that a logo is produced.
+* Add data validation to the form.
+* Add more information to the home page in the form of media or a small description.
---
+++ js/script.js
@@ -0,0 +1,61 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+let userInput, stockData, stockData2;
+
+/*----- cached element references -----*/
+const $input = $('input[type="text"]');
+const $logo = $('#logo');
+const $name = $('#name');
+const $price = $('#price');
+const $daily = $('#daily');
+
+/*----- event listeners -----*/
+$('form').on('submit', handleGetData);
+
+/*----- functions -----*/
+function handleGetData(e) {
+    // clearData();
+    e.preventDefault();
+
+    if($input.val() === "") return;
+    userInput = $input.val();
+
+    $input.val("");
+
+    $.ajax({
+        url:'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + userInput + '&apikey=UYEFNB9NIZ2OB7BW'
+    }).then(function(data) {
+        stockData = data["Global Quote"];
+        render();
+    }, function(error){
+        console.log('Bad first request: ', error);
+    }
+    );
+
+    $.ajax({
+        url:'https://api.worldtradingdata.com/api/v1/stock?symbol=' + userInput + '&api_token=auG6dvpnFIvffbLimM1xD5CzdwXXas6qDQmrMvWBbdoSmS81zG4yLbLTgztF'
+    }).then(function(data) {
+        stockData2 = data.data[0];
+        render2();
+    }, function(error) {
+        console.log('Bad second request: ', error);
+    }
+    );
+}
+
+function render() {
+    $price.html("$" + parseFloat(stockData["05. price"]).toFixed(2));
+    $daily.html("$" + parseFloat(stockData["09. change"]).toFixed(2));
+}
+
+function render2() {
+    $name.html(stockData2.name);
+}
+
+function clearData() {
+    $name.html("");
+    $price.html("");
+    $daily.html("");
+    logo.html("FIX ME ADD IMG");
+}
